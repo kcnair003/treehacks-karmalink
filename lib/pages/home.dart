f8c5301535e05e820174a9c15c0a5ae2a11d227a
Steven Wu <swu0430@gmail.com>
2021-02-13 17:02:11 -0500

    Added shading

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:treehacks2021/dynamicmodels/ThemeSelection.dart';
 
 class Home extends StatelessWidget {
   bool isAuth = false;
@@ -8,28 +10,30 @@ class Home extends StatelessWidget {
   }
 
   Widget buildNoAuthScreen() {
-    return Column(
-      children: [
-        Consumer<ThemeNotifier>(
-            builder: (context, model, space) => Switch(
-                value: model.switchUse,
-                onChanged: (switchUse) => model.toggle())),
-        Image(
-          image: AssetImage('assets/images/logo.png'),
-          width: 500,
-          height: 250,
-        ),
-        GestureDetector(
-            child: Container(
-          width: 260,
-          height: 60,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/signinBtn.png'),
-            ),
+    return Scaffold (
+      body: Column(
+        children: <Widget>[
+          Consumer<ThemeNotifier>(
+              builder: (context, model, space) => Switch(
+                  value: model.switchUse,
+                  onChanged: (switchUse) => model.toggle())),
+          Image(
+            image: AssetImage('assets/images/logo.png'),
+            width: 500,
+            height: 250,
           ),
-        ))
-      ],
+          GestureDetector(
+              child: Container(
+            width: 260,
+            height: 60,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/signinBtn.png'),
+              ),
+            ),
+          ))
+        ],
+      )
     );
 
     // return Column(

05f31e87acb304b755edf8323b3cbcc86ff3ddb4
Phyo Khine <pkhine.2020@berkeley.edu>
2021-02-13 13:20:53 -0800

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import './pages/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

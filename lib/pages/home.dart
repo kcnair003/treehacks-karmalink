b0a924d5be3c35231f533d02452692ec39b4ab8c
Phyo Khine <pkhine.2020@berkeley.edu>
2021-02-14 04:03:54 -0800

    almost done with feeds view

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,6 +14,7 @@ import 'package:treehacks2021/widgets/nav_bar_button.dart';
 import 'package:treehacks2021/widgets/nav_bar_item.dart';
 import 'package:treehacks2021/dynamicmodels/ThemeSelection.dart';
 import 'package:provider/provider.dart';
+import 'package:treehacks2021/widgets/postcard.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
 final postsRef = FirebaseFirestore.instance.collection('posts');
@@ -134,7 +135,8 @@ class _HomeState extends State<Home> {
               print(index);
               print("above is index");
               print(posts[index]);
-              return ListTile(title: Text(posts[index]["message"]));
+              return PostCard(
+                  posts[index]["message"], posts[index]["username"]);
             }),
       ),
     ));
@@ -148,52 +150,71 @@ class _HomeState extends State<Home> {
           color: Colors.lightGreen,
           child: Column(
             children: [
-              Container(
-                width: showChat ? 200 : double.infinity,
-                height: 200,
-              ),
-              TextField(
-                onChanged: (value) {
-                  setState(() {
-                    pendingPost = value;
-                  });
-                  print(pendingPost);
-                },
-                decoration: InputDecoration(
-                  hintText: 'Write Something...',
-                  contentPadding: EdgeInsets.all(20.0),
+              // Container(
+              //   width: showChat ? 200 : double.infinity,
+              //   height: 200,
+              // ),
+              new Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    border: new Border.all(
+                      color: Colors.black,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: new TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    textAlign: TextAlign.left,
+                    decoration: new InputDecoration(
+                        hintText: 'Write Something To Post...',
+                        contentPadding: EdgeInsets.all(15),
+                        border: InputBorder.none,
+                        labelStyle: TextStyle(fontSize: 20)),
+                    // hintStyle: TextStyle(fontSize: 20)),
+                  ),
                 ),
               ),
-              OutlinedButton(
-                onPressed: () {
-                  final currUser = FirebaseAuth.instance.currentUser;
-                  if (pendingPost != "") {
-                    print("OutlineddButton onPressed");
-                    print(currUser);
-                    print(isAuth);
-                    print(pendingPost);
-                    usersRef.doc(currUser.uid).get().then((value) {
-                      postsRef.add({
-                        "message": pendingPost,
-                        "user_id": currUser.uid,
-                        "time_posted": DateTime.now(),
-                        "username": value.data()["username"]
+
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextButton(
+                  onPressed: () {
+                    final currUser = FirebaseAuth.instance.currentUser;
+                    if (pendingPost != "") {
+                      print("OutlineddButton onPressed");
+                      print(currUser);
+                      print(isAuth);
+                      print(pendingPost);
+                      usersRef.doc(currUser.uid).get().then((value) {
+                        postsRef.add({
+                          "message": pendingPost,
+                          "user_id": currUser.uid,
+                          "time_posted": DateTime.now(),
+                          "username": value.data()["username"]
+                        });
                       });
-                    });
-                    // print("Post ADDED TO FIREBASE");
-                  }
-                },
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.post_add_rounded,
-                      color: Colors.black,
-                    ),
-                    Text(
-                      "Make a Post",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
+                      // print("Post ADDED TO FIREBASE");
+                    }
+                  },
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.post_add_rounded,
+                        color: Colors.black,
+                      ),
+                      Text(
+                        "Make a Post",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               // Container(
@@ -242,13 +263,19 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text('Karmalink or DialogueDen'),
         actions: [
-          GestureDetector(
-            onTap: manageWidgets,
-            child: Icon(Icons.post_add_rounded),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: GestureDetector(
+              onTap: manageWidgets,
+              child: Icon(Icons.post_add_rounded),
+            ),
           ),
-          GestureDetector(
-            onTap: manageWidgets,
-            child: Icon(Icons.chat_bubble),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: GestureDetector(
+              onTap: manageWidgets,
+              child: Icon(Icons.chat_bubble),
+            ),
           ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 16),
---
+++ lib/widgets/postcard.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget {
+  PostCard(this.content, this.poster);
+  final String content;
+  final String poster;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(22, 15, 22, 15),
+        child: Row(children: [
+          Icon(
+            Icons.person,
+            size: 60,
+            color: Colors.black,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(poster,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.black54)),
+              SizedBox(
+                height: 3,
+              ),
+              Text(
+                content,
+                textScaleFactor: 1.2,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.how_to_vote_outlined),
+                  Icon(Icons.star_rate_rounded),
+                ],
+              )
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}

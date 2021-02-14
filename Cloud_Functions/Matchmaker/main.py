d65f1da26837ff1f363237b8d8466b9dcf7348b4
kcnair003 <kcnair003@gmail.com>
2021-02-14 12:59:04 -0600

    Update main.py

--- Cloud_Functions/Matchmaker/main.py
+++ Cloud_Functions/Matchmaker/main.py
@@ -44,7 +44,7 @@ def create_groups(tupled_user_list):
         list_of_keys.append(key)
         list_of_values.append(value[1])
     weights_norm = np.linalg.norm(np.array(list_of_values))
-    weights_normed = np.array([abs(weight/weights_norm) for weight in list_of_values])
+    weights_normed = np.array([np.nan_to_num(abs(weight/weights_norm)) for weight in list_of_values])
     weights_array = weights_normed / weights_normed.sum()
     weights_reduced = np.nan_to_num(weights_array)
     weights_scaled = weights_reduced.tolist()

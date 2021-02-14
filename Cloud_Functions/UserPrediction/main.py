e1035543abc94ceac7de5c2f7f7efb2e97ed2b37
kcnair003 <kcnair003@gmail.com>
2021-02-14 03:49:12 -0600

    Fixed model parameter handling

--- Cloud_Functions/UserPrediction/main.py
+++ Cloud_Functions/UserPrediction/main.py
@@ -1,9 +1,6 @@
 from __future__ import print_function
 import json
-import nltk
-from nltk.corpus import stopwords
 import pandas as pd
-import spacy
 import string
 from google.cloud import firestore
 import datetime
@@ -13,36 +10,48 @@ import base64
 from google.oauth2 import service_account
 import os
 from google.cloud import language_v1
+import googleapiclient.discovery
 from googleapiclient.discovery import build
 from googleapiclient import errors
 import traceback
 
-import sys
-sys.path.append('/user_code/SentimentAnalysis.py')
-from SentimentAnalysis import analyze_blob_sentiment, analyze_blob_subjectivity, analyze_vader_sentiment
-
 # Establishes the Firestore client
 db = firestore.Client()
 
-def analyze_cloud_sentiment(text_phrase):
-    # Defines the client, type of document, and the language used in the sentiment analysis
-    client = language_v1.LanguageServiceClient()
-    language = "en"
-    document = language_v1.Document(content=text_phrase, type_=language_v1.Document.Type.PLAIN_TEXT, language=language)
-    response = client.analyze_sentiment(request={'document': document})
-    # Uses the analyze_sentiment() method to find the overall polarity and magnitude of the text
-    text_polarity = response.document_sentiment.score
-    return(text_polarity)
+PROJECT_ID = "treehacks-karmalink"
+VERSION_NAME = "Version_2"
+MODEL_NAME = "Sentiment_KNN"
 
-#Combines different sentiment methods into one data dict
-def combine_sentiment_methods(message):
-    cloud_polarity = analyze_cloud_sentiment(message)
-    blob_polarity = analyze_blob_sentiment(message)
-    blob_subjectivity = analyze_blob_subjectivity(message)
-    vader_polarity = analyze_vader_sentiment(message)
-    data = {'Cloud_Polarity': cloud_polarity, 'TextBlob_Polarity': blob_polarity, 'TextBlob_Subjectivity': blob_subjectivity, 'VADER_Polarity': vader_polarity}
-    return data
+def test_prediction_model(test_array, test_labels):
 
+    service = googleapiclient.discovery.build('ml', 'v1')
+    name = 'projects/{}/models/{}'.format(PROJECT_ID, MODEL_NAME)
+    name += '/versions/{}'.format(VERSION_NAME)
+
+    X_test = test_array
+    y_test = test_labels
+
+    # Due to the size of the data, it needs to be split in 2
+    first_half = X_test[:int(len(X_test)/2)]
+    second_half = X_test[int(len(X_test)/2):]
+
+    complete_results = []
+    for data in [first_half, second_half]:
+        responses = service.projects().predict(
+            name=name,
+            body={'instances': data}
+        ).execute()
+
+        if 'error' in responses:
+            print(response['error'])
+        else:
+            complete_results.extend(responses['predictions'])
+
+    # Print the first 10 responses
+    datadict=[]
+    for i, response in enumerate(complete_results[:10]):
+        datadict.append('Prediction: {}\tLabel: {}'.format(response, y_test[i]))
+    return datadict
 
 def calculate_polarity(request):
     """Responds to any HTTP request.
@@ -54,13 +63,19 @@ def calculate_polarity(request):
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
     request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        message = request.args.get('message')
-        data = combine_sentiment_methods(message)
+    if request.args and 'X_test' in request.args and 'y_test' in request.args:
+        scores = request.args.get('X_test')
+        labels = request.args.get('y_test')
+        df = pd.json_normalize(scores)
+        df2 = pd.json_normalize(labels)
+        data = test_prediction_model(df, df2)
         return data
-    elif request_json and 'message' in request_json:
-        message = request_json['message']
-        data = combine_sentiment_methods(message)
+    elif request_json and 'X_test' in request_json and 'y_test' in request_json:
+        scores = request_json['X_test']
+        labels = request_json['y_test']
+        df = pd.json_normalize(scores)
+        df2 = pd.json_normalize(labels)
+        data = test_prediction_model(df, df2)
         return data
     else:
         return f'The proper input was not given'
